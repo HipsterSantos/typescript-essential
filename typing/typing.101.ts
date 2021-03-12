91d023f68dd66d6808a9986a16c4902c35cc8599
HipsterSantos <santoscampos269@gmail.com>
2021-03-12 19:01:04 +0100

    Typing 101 {

---
+++ typing/typing.101.ts
@@ -0,0 +1,15 @@
+//Type anntations 
+// What we'll approach here: 
+-Constants  && variables
+-Enumarations && Constant Enumaration
+-Union Types 
+-Literal Types
+-Insertion Types
+-Array and typing 
+-Tuple Types
+-Dictionary Types 
+-Mapped Types 
+-Type Assertions
+-Type Guards 
+-Discriminarted Unions
+-Destructuring && Spreading 
